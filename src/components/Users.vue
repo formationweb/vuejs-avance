cfe4a239b47567a144db8147f7f10c59b4006c33
RSamaium <sam@newbot.io>
2026-04-13 10:12:37 +0200

    init

---
+++ src/components/Users.vue
@@ -0,0 +1,16 @@
+<template>
+    <article v-for="user in users" :key="user.id">
+        <header>{{ user.name }}</header>
+        <p>{{ user.email }}</p>
+    </article>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const users = ref<any>([])
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res) => res.json())
+    .then((data) => users.value = data)
+</script>
